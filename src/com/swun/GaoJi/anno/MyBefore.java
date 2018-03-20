a8ed6439d292fc9c53854900813aeffbc00c2673
meifan <meifan_mvc@foxmail.com>
2018-03-20 21:53:16 +0800

    anno

--- src/com/swun/GaoJi/anno/MyBefore.java
+++ src/com/swun/GaoJi/anno/MyBefore.java
@@ -1,18 +1,11 @@
-package com.swun.GaoJi.Annotation;
+package com.swun.GaoJi.anno;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * 描述:
- * 自定义注解
- * @outhor mf
- * @create 2018-03-19 15:14
- */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface myTest {
-
+public @interface MyBefore {
 }
