3b445426f927bdad9fb80db7d30ccbc86eb0e85b
mf <meifan_cd@keruyun.com>
2018-04-19 14:38:53 +0800

    nio

---
+++ src/com/swun/nio/NioTest1.java
@@ -0,0 +1,21 @@
+package com.swun.nio;
+
+import org.junit.Test;
+
+
+import java.nio.ByteBuffer;
+
+/**
+ * 描述:
+ *
+ * @outhor my
+ * @create 2018-04-19 14:37
+ */
+public class NioTest1 {
+    @Test
+    public void test1(){
+
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
+
+    }
+}
