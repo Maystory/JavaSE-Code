da09d2f475b382de10f2798977b98ae7508c808a
mf <meifan_cd@keruyun.com>
2018-06-05 17:10:59 +0800

    test

--- src/com/swun/GaoJi/TestDemo.java
+++ src/com/swun/GaoJi/TestDemo.java
@@ -1,5 +1,6 @@
 package com.swun.GaoJi;
 
+import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -11,11 +12,23 @@ import java.util.Date;
  */
 public class TestDemo {
 
-    public static void main(String[] args)throws Exception {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    public static void main(String[] args) throws Exception {
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+//
+//        Date date = simpleDateFormat.parse("2017-08-01");
+//        System.out.println(date);
+//        String str = "1995-08-01";
+//        str =str.substring(0,str.lastIndexOf("-"));
+//        System.out.println(str);
 
-        Date date = simpleDateFormat.parse("2017-08-01");
-        System.out.println(date);
+//        BigDecimal b1 = new BigDecimal("0.1");
+//        System.out.println(b1);
+//
+//        System.out.println(new BigDecimal(2).divide(new BigDecimal(3),2,BigDecimal.ROUND_HALF_DOWN));
+        //  Double d1 = 1.0;
+        // Double d2 = 3.0;
+
+        // new BigDecimal("453.433543",BigDecimal.ROUND_DOWN,4)
     }
 
 }
