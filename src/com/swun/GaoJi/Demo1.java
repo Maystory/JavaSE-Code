f7b4488e85fcea94b753bab2c8832aadd419372f
mf <meifan_cd@keruyun.com>
2018-06-06 10:59:32 +0800

    demo

---
+++ src/com/swun/GaoJi/Demo1.java
@@ -0,0 +1,104 @@
+package com.swun.GaoJi;
+
+import org.joda.time.*;
+
+/**
+ * 描述:
+ *
+ * @outhor my
+ * @create 2018-05-31 11:25
+ */
+public class Demo1 {
+
+    public static void main(String[] args) throws Exception {
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+//
+//        Date date = simpleDateFormat.parse("2017-08-01");
+//        System.out.println(date);
+//        String str = "1995-08-01";
+//        str =str.substring(0,str.lastIndexOf("-"));
+//        System.out.println(str);
+
+//        BigDecimal b1 = new BigDecimal("0.1");
+//        System.out.println(b1);
+//
+//        System.out.println(new BigDecimal(2).divide(new BigDecimal(3),2,BigDecimal.ROUND_HALF_DOWN));
+        //  Double d1 = 1.0;
+        // Double d2 = 3.0;
+
+        // new BigDecimal("453.433543",BigDecimal.ROUND_DOWN,4)
+//        Runtime runtime = Runtime.getRuntime();
+//        System.out.println(runtime.availableProcessors());
+//        System.out.println(runtime.freeMemory());
+//        System.out.println(runtime.totalMemory());
+//
+//        Object o = new Object();
+//        Object o2=null;
+//        Double d1= 6.45;
+//        System.out.println(Objects.hashCode(o));
+//        System.out.println(Objects.requireNonNull(o));
+//        BigDecimal bigDecimal = BigDecimal.valueOf(d1);
+//        System.out.println(bigDecimal.divide(BigDecimal.valueOf(1.33),3,BigDecimal.ROUND_HALF_DOWN));
+//        long l = System.currentTimeMillis();
+//        System.out.println(l);
+//        Date date = new Date();
+//        //在某个日期之前
+//        System.out.println(date.after(new Date(Long.parseLong("1528198181724"))));
+
+//        Calendar calendar = Calendar.getInstance();
+//        Date time = calendar.getTime();
+//        System.out.println(time);
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//        System.out.println(simpleDateFormat.format(time));
+//        calendar.setTime(new Date());
+//
+//        System.out.println(calendar.get(Calendar.DAY_OF_MONTH));
+//        calendar.add(Calendar.YEAR,-1);
+//        System.out.println(calendar.get(Calendar.YEAR));
+//        DateTime dateTime = new DateTime(2018,6,6,11,11,11);
+//        System.out.println(dateTime.toString(" E yyyy-MM-dd HH:mm:ss"));  //星期三 2018-06-06 11:11:11
+
+//        System.out.println(dateTime.plusDays(90).toString(" E yyyy-MM-dd HH:mm:ss"));
+//        System.out.println(dateTime.plusDays(45).plusMonths(2).dayOfWeek().withMinimumValue().toString(" E yyyy-MM-dd HH:mm:ss"));
+
+//        DateTime dateTime = new DateTime();
+//        System.out.println(dateTime.toString("yyyy/MM/dd HH:mm:ss EE"));
+//        Days days = Days.daysBetween(dateTime, dateTime.plusYears(1).withDayOfYear(1));
+//        System.out.println(days.getDays());
+//
+//        DateTime begin = new DateTime("2015-02-01");
+//        DateTime end = new DateTime("2016-05-01");
+//        Duration duration = new Duration(begin, end);
+//        System.out.println(duration.getStandardDays());
+//        /**
+//         * //计算区间天数
+//         */
+//        Period period = new Period(begin, end, PeriodType.days());
+//        System.out.println(period.getDays());
+//        /**
+//         * //计算特定日期是否在该区间内
+//         */
+//        Interval interval = new Interval(begin, end);
+//        System.out.println(interval.contains(new DateTime(2018, 8, 1, 1, 1)));
+
+
+//        DateTime d1 = new DateTime("2015-10-01");
+//        DateTime d2 = new DateTime("2016-02-01");
+//
+//        //和系统时间比
+//        boolean b1 = d1.isAfterNow();
+//        boolean b2 = d1.isBeforeNow();
+//        boolean b3 = d1.isEqualNow();
+//
+//        //和其他日期比
+//        boolean f1 = d1.isAfter(d2);
+//        boolean f2 = d1.isBefore(d2);
+//        boolean f3 = d1.isEqual(d2);
+
+//         String uuidName = "你好发[iowriweor45]";
+//        System.out.println(uuidName.substring(0,uuidName.indexOf("[")));
+
+    }
+
+
+}
