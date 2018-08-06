44ddb6fd01ca9051aa284c2ea85600caec05ba02
meifan <meifan_mvc@foxmail.com>
2018-08-06 21:14:16 +0800

    think java

---
+++ src/com/swun/thinkinjava/chapter14/Gum.java
@@ -0,0 +1,13 @@
+package com.swun.thinkinjava.chapter14;
+
+import static jdk.nashorn.internal.objects.Global.print;
+
+/**
+ * Created by Maven on 2018/8/1.
+ * Description:
+ */
+public class Gum {
+    static {
+        print("loading Gum");
+    }
+}
