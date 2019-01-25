f7e7660cbefe3b712cca53291ab5490fc3e28be9
meifan <meifan_mvc@foxmail.com>
2019-01-25 14:49:46 +0800

    reverInter

---
+++ src/com/letcode/easy/NumberIsPalindrome.java
@@ -0,0 +1,43 @@
+package com.letcode.easy;
+
+import java.util.Objects;
+
+/**
+ * @author Maven
+ * @since 2019-01-25 14:33
+ *
+ *
+ * Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+ *
+ * Example 1:
+ *
+ * Input: 121
+ * Output: true
+ * Example 2:
+ *
+ * Input: -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+ * Example 3:
+ *
+ * Input: 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ *
+ */
+public class NumberIsPalindrome {
+
+    public static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        StringBuilder reverse = new StringBuilder(String.valueOf(x)).reverse();
+        return String.valueOf(x).equals(reverse.toString());
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(123));
+
+    }
+}
--- src/com/letcode/easy/ReversInteger.java
+++ src/com/letcode/easy/ReversInteger.java
@@ -6,13 +6,20 @@ import javax.xml.bind.SchemaOutputResolver;
  * Created by Maven on 2018/8/31.
  * Description:
  * <p>
- * <p>
- * Given a 32-bit signed integer, reverse digits of an integer.
- * <p>
  * Example 1:
  * <p>
  * Input: 123
  * Output: 321
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: -123
+ * Output: -321
+ * <p>
+ * Example 3:
+ * <p>
+ * Input: 120
+ * Output: 21
  */
 public class ReversInteger {
 
@@ -53,8 +60,28 @@ public class ReversInteger {
         }
     }
 
+    public static int reverse2(int x) {
+        //最终结果
+        int rev = 0;
+        while (x != 0) {
+            int top = x % 10;
+            //防止溢出
+            if (rev > Integer.MAX_VALUE/10 || (rev ==Integer.MAX_VALUE/10 && top >7)) {
+                return 0;
+            }
+            if (rev < Integer.MIN_VALUE/10 || (rev ==Integer.MIN_VALUE /10&& top <-8)) {
+                return 0;
+            }
+            //最后一位
+            rev = rev * 10 + top;
+            x = x / 10;
+        }
+        return rev;
+    }
+
     public static void main(String[] args) {
-        System.out.println(Integer.MAX_VALUE);
-        System.out.println(reverse(1534236469));
+        // System.out.println(Integer.MAX_VALUE);
+        // System.out.println(reverse(1534236469));
+        System.out.println(reverse2(123));
     }
 }
